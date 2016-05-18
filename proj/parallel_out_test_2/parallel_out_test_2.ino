3698a3f760377ffd771bc1dbcf8b4e48629e9f6a
Eli Woods <eawoods3@gmail.com>
2016-05-17 22:02:45 -0700

    updated parallel out 2

--- proj/parallel_out_test_2/parallel_out_test_2.ino
+++ proj/parallel_out_test_2/parallel_out_test_2.ino
@@ -11,6 +11,9 @@ CRGB leds[NUM_STRIPS * NUM_LED];
 // WS2811_PORTD: 25,26,27,28,14,15,29,11
 //
 
+CRGBPalette16 gPalette;
+uint8_t gHue;
+
 void setup() {
   LEDS.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LED);
   LEDS.setBrightness(32);
@@ -19,15 +22,47 @@ void setup() {
 }
 
 void loop() {
-  static uint8_t hue = 0;
+
+  EVERY_N_MILLISECONDS(10) {
+    gHue++;
+  }
+
+  update_RainbowBlack_p();
+  theater_chase();
+  
+  // Set the first n leds on each strip to show which strip it is
   for (int i = 0; i < NUM_STRIPS; i++) {
-    for (int j = 0; j < NUM_LED; j++) {
-      leds[(i * NUM_LED) + j] = CHSV((32 * i) + hue + j, 192, 255);
+    for (int j = 0; j <= i; j++) {
+      leds[(i * NUM_LED) + j] = CRGB::Red;
     }
   }
 
-  hue++;
 
   LEDS.show();
   LEDS.delay(10);
+}
+
+void theater_chase() {
+  static uint8_t pal_index = 0;
+  EVERY_N_MILLISECONDS(100) {
+    pal_index++;
+  }
+
+  for (uint8_t ss = 0; ss < NUM_STRIPS; ss++) {
+    fill_palette(&(leds[ss*NUM_LED]), NUM_LED, pal_index, 6, gPalette, 60, LINEARBLEND);
+  }
+
+}
+
+// RB|B|B|B repeating
+void update_RainbowBlack_p() {
+  // Instead of blacking out, fill backgroud with rainbow, then
+  // fill every thing with black except
+  fill_rainbow(gPalette, 16, gHue, 10);
+  for (uint8_t i = 0 ; i < 16; i++) {
+    if ( i % 4 == 0) {
+      continue;
+    }
+    gPalette[i] = CRGB::Black;
+  }
 }
