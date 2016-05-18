7b3d8a6a871109ec5cf97326a1ef916791053566
Eli Woods <eawoods3@gmail.com>
2016-05-17 21:42:30 -0700

    updated parallel out 2

--- proj/parallel_out_test_2/parallel_out_test_2.ino
+++ proj/parallel_out_test_2/parallel_out_test_2.ino
@@ -11,26 +11,6 @@ CRGB leds[NUM_STRIPS * NUM_LED];
 // WS2811_PORTD: 25,26,27,28,14,15,29,11
 //
 
-const TProgmemPalette16 WhiteBlack_p PROGMEM = {
-  CRGB::Red,
-  CRGB::Blue,
-  CRGB::Orange,
-  CRGB::Purple,
-  CRGB::Yellow,
-  CRGB::Green,
-  CRGB::White,
-  CRGB::Pink,
-  CRGB::White,
-  CRGB::Black,
-  CRGB::Black,
-  CRGB::Black,
-  CRGB::White,
-  CRGB::Black,
-  CRGB::Black,
-  CRGB::Black,
-};
-
-
 void setup() {
   // LEDS.addLeds<WS2811_PORTA,NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
   // LEDS.addLeds<WS2811_PORTB,NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
@@ -43,9 +23,9 @@ void setup() {
 
 void loop() {
 
-  for (uint8_t strip = 0; strip < NUM_STRIPS; strip++) {
-    for (uint8_t ll = 0; ll < NUM_LED; ll++) {
-      leds[strip * NUM_LED + ll] = CHSV(strip*2*16, 192, 255);
+  for (int i = 0; i < NUM_STRIPS; i++) {
+    for (int j = 0; j < NUM_LED; j++) {
+      leds[(i * NUM_LED) + j] = CHSV((32 * i), 192, 255);
     }
   }
 
