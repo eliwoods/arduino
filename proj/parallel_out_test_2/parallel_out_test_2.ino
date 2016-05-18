7ba5cf47158f7167409e19d15b6840f566c9ff9b
Eli Woods <eawoods3@gmail.com>
2016-05-17 20:52:13 -0700

    added new parallel test

--- proj/parallel_out_test_2/parallel_out_test_2.ino
+++ proj/parallel_out_test_2/parallel_out_test_2.ino
@@ -33,6 +33,9 @@ const TProgmemPalette16 WhiteBlack_p PROGMEM = {
   CRGB::Black,
 };
 
+CRGBPalette16 gPalette;
+uint8_t gHue;
+
 void setup() {
   // LEDS.addLeds<WS2811_PORTA,NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
   // LEDS.addLeds<WS2811_PORTB,NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
@@ -40,33 +43,49 @@ void setup() {
   LEDS.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LED);
   LEDS.setBrightness(32);
 
-  LEDS.setMaxPowerInMilliWatts(24000);
+  //LEDS.setMaxPowerInMilliWatts(24000);
 }
 
 void loop() {
 
 
-//  for (uint8_t ss = 0; ss < NUM_STRIPS; ss++) {
-//    fill_solid(&(leds[ss*NUM_LED]), NUM_LED, CHSV(2*ss*16, 192, 255)); 
-//  }
+  //  for (uint8_t ss = 0; ss < NUM_STRIPS; ss++) {
+  //    fill_solid(&(leds[ss*NUM_LED]), NUM_LED, CHSV(2*ss*16, 192, 255));
+  //  }
 
-  static uint8_t hue = 0;
-  for (int i = 0; i < NUM_STRIPS; i++) {
-    for (int j = 0; j < NUM_LED; j++) {
-      leds[(i * NUM_LED) + j] = CHSV((32 * i) + hue + j, 192, 255);
-    }
+  EVERY_N_MILLISECONDS(10) {
+    gHue++;
   }
 
-  // Set the first n leds on each strip to show which strip it is
-  for (int i = 0; i < NUM_STRIPS; i++) {
-    for (int j = 0; j <= i; j++) {
-      leds[(i * NUM_LED) + j] = CRGB::Red;
-    }
-  }
+  update_RainbowBlack_p();
+  theater_chase();
 
-  hue++;
 
   //LEDS.setBrightness(32);
+  LEDS.delay(100);
+}
+
+void theater_chase() {
+  static uint8_t pal_index = 0;
+  EVERY_N_MILLISECONDS(100) {
+    pal_index++;
+  }
+
+  for (uint8_t ss = 0; ss < NUM_STRIPS; ss ++) {
+    fill_palette(leds + (ss * NUM_LED), NUM_LED, pal_index, 6, gPalette, 60, LINEARBLEND);
+  }
   LEDS.show();
-  LEDS.delay(10);
+}
+
+// RB|B|B|B repeating
+void update_RainbowBlack_p() {
+  // Instead of blacking out, fill backgroud with rainbow, then
+  // fill every thing with black except
+  fill_rainbow(gPalette, 16, gHue, 10);
+  for (uint8_t i = 0 ; i < 16; i++) {
+    if ( i % 4 == 0) {
+      continue;
+    }
+    gPalette[i] = CRGB::Black;
+  }
 }
