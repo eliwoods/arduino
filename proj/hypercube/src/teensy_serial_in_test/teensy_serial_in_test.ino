cc7beabdc5b495ee7aeb657d4fc718c214b451e2
Eli Woods <eawoods3@gmail.com>
2016-08-06 18:08:46 -0700

    Updated teensy serial test

--- proj/hypercube/src/teensy_serial_in_test/teensy_serial_in_test.ino
+++ proj/hypercube/src/teensy_serial_in_test/teensy_serial_in_test.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
 #define NUM_LED 30
-#define LED_PIN 1
+#define LED_PIN 5
 
 CRGB leds[NUM_LED];
 
@@ -10,9 +10,8 @@ boolean got_event = false;
 
 void setup() {
   // Setup Serial
-  Serial2.begin(9600);
-
-  pinMode(0, OUTPUT);
+  Serial1.begin(9600);
+  Serial.begin(9600);
 
   // Setup FastLED
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LED);
@@ -23,20 +22,35 @@ void setup() {
 
 void loop() {
 
-  fill_solid(leds, NUM_LED, CHSV(gHue, 255, 100));
-  FastLED.show();
-  FastLED.delay(10); // idk why I'm doing this..
-    delay(1000);
+  if ( Serial1.available() > 0) {
+    gHue = Serial1.read();
+    fill_solid(leds, NUM_LED, CHSV(gHue, 255, 100));
+    FastLED.show();
+    FastLED.delay(10); // idk why I'm doing this..
+  }
 
+
+  //    if (got_event) {
+  //      fill_solid(leds, NUM_LED, CHSV(gHue, 255, 100));
+  //      FastLED.show();
+  //      FastLED.delay(10); // idk why I'm doing this..
+  //      got_event = false;
+  //    }
+
+  //  fill_solid(leds, NUM_LED, CHSV(gHue, 255, 100));
+  //  FastLED.show();
+  //  FastLED.delay(10); // idk why I'm doing this..
+  //  gHue++;
 
 }
 
 void serialEvent() {
-  while (Serial2.available()) {
-    gHue = Serial2.read();
-    if ((char)Serial2.read() == '\n') {
-      got_event = true;
-    }
+  while (Serial1.available()) {
+    gHue = Serial1.read();
+    //    if ((char)Serial.read() == '\n') {
+    //      got_event = true;
+    //    }
+    got_event = true;
   }
 }
 
