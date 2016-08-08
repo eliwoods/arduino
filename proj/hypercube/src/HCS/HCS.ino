31f2bf96c91b0feb2e7a46e84a7a8c67f3cc338a
Eli Woods <eawoods3@gmail.com>
2016-08-08 00:39:02 -0700

    HCS is in a state that works with the rig. Also added wiring diagram for the whole rig

--- proj/hypercube/src/HCS/HCS.ino
+++ proj/hypercube/src/HCS/HCS.ino
@@ -12,8 +12,8 @@
 const uint16_t strip_len = 90; // Number of LED per strip
 const uint8_t num_per_group = 6; // Number of strips per OctoWS2811 group
 
-const uint8_t in_strips = 18; // Number of strips on inner shell
-const uint8_t out_strips = 24; // Number of strips on outer shell
+const uint8_t in_strips = 3 * num_per_group; // Number of strips on inner shell
+const uint8_t out_strips = 5 * num_per_group; // Number of strips on outer shell
 
 const uint16_t in_LED_tot = in_strips * strip_len;
 const uint16_t out_LED_tot = out_strips * strip_len;
@@ -28,7 +28,7 @@ CRGBArray<out_LED_tot> out_leds;
 CRGBArray<led_tot> leds;
 
 // For controlling the brightness, again might not need this variability.
-const uint8_t maxBrightness = 150;
+const uint8_t maxBrightness = 50;
 uint8_t gBrightness = maxBrightness; // CHANGE THIS ONCE YOU HAVE ANOTHER POTENTIOMETER
 
 // Setup and global variable delcaration for palettes
@@ -47,13 +47,13 @@ uint8_t chaser_opt = 0; // For choosing the sub patterns on the animations that 
 
 void setup() {
   // Initialize the leds, specifially to use OctoWS2811 controller
-  LEDS.addLeds<OCTOWS2811>(leds, strip_len); // No need to declare pin numbers since they are preset with parallel output
-  LEDS.setBrightness(200); // LETS SEE HOW FAR WE CAN PUSH IT
+  LEDS.addLeds<OCTOWS2811>(leds, strip_len * num_per_group); // No need to declare pin numbers since they are preset with parallel output
+  LEDS.setBrightness(maxBrightness); // LETS SEE HOW FAR WE CAN PUSH IT
   LEDS.show();
 
   // Initialize global color variables
   gBrightness = maxBrightness;
-  gBlending = LINEARBLEND;
+  gBlending = NOBLEND;
   gPalette = RainbowColors_p; // SHOULD CHANGE THIS
   gIndex = 0;
   gHue = 0;
@@ -62,143 +62,171 @@ void setup() {
 
 void loop() {
   static uint8_t gAnimCounter = 0;
-  EVERY_N_SECONDS(10) {
-    gAnimCounter = (gAnimCounter + 1) % 6;
+  EVERY_N_SECONDS(30) {
+    gAnimCounter = (gAnimCounter + 1) % 2;
   }
 
   // Since it costs basically nothing, lets change the global index on each looop
-  EVERY_N_MILLISECONDS(100) {
+  EVERY_N_MILLISECONDS(1) {
     gIndex++;
   }
 
-  if (gAnimCounter == 0) {
-    // First let's try filling each group with a different color based on Ethan's diagram
-    fill_solid(leds(strip_len * 0 * num_per_group, strip_len * 1 * num_per_group - 1), strip_len * num_per_group, CRGB::Yellow);
-    fill_solid(leds(strip_len * 1 * num_per_group, strip_len * 2 * num_per_group - 1), strip_len * num_per_group, CRGB::Pink);
-    fill_solid(leds(strip_len * 2 * num_per_group, strip_len * 3 * num_per_group - 1), strip_len * num_per_group, CRGB::Cyan);
-    fill_solid(leds(strip_len * 3 * num_per_group, strip_len * 4 * num_per_group - 1), strip_len * num_per_group, CRGB::Green);
-    fill_solid(leds(strip_len * 4 * num_per_group, strip_len * 5 * num_per_group - 1), strip_len * num_per_group, CRGB::Purple);
-    fill_solid(leds(strip_len * 5 * num_per_group, strip_len * 6 * num_per_group - 1), strip_len * num_per_group, CRGB::Blue);
-    fill_solid(leds(strip_len * 6 * num_per_group, strip_len * 7 * num_per_group - 1), strip_len * num_per_group, CRGB::Orange);
-    fill_solid(leds(strip_len * 7 * num_per_group, strip_len * 8 * num_per_group - 1), strip_len * num_per_group, CRGB::Red);
-    LEDS.show();
+  switch (gAnimCounter) {
+    // Now lets see if we can send chasers all in one direction along each shell
+    case 0:
+      // Fill the template array first
+      chaser(led_tmplt, strip_len, gIndex);
+
+      // Now fill both the inner template
+      for (uint8_t s = 0; s < in_strips; s++) {
+        if (s % 2 == 0) {
+          in_leds(strip_len * s, strip_len * (s + 1) - 1) = led_tmplt;
+        }
+        else {
+          in_leds(strip_len * (s + 1) - 1, strip_len * s) = led_tmplt;
+        }
+      }
+
+      // Fill outer template
+      for (uint8_t s = 0; s < out_strips; s++) {
+        if (s % 2 == 0) {
+          out_leds(strip_len * (s + 1) - 1, strip_len * s) = led_tmplt;
+        }
+        else {
+          out_leds(strip_len * s, strip_len * (s + 1) - 1) = led_tmplt;
+        }
+      }
+
+      // Now send to the LEDs
+      copy_pasta_dump();
+      break;
+  
+    case 1:
+      static uint8_t in_offset = 12;
+      static uint8_t out_offset = 4;
+      // First do the inner cylider since it's smaller
+      for (uint8_t s = 0; s < in_strips; s++) {
+        chaser(led_tmplt, strip_len, gIndex + in_offset * s);
+        if (s % 2 == 0) {
+          in_leds(s * strip_len, strip_len * (s + 1) - 1) = led_tmplt;
+        }
+        else {
+          in_leds(strip_len * (s + 1) - 1, s * strip_len) = led_tmplt;
+        }
+      }
+
+      // Next do the outer cylinder
+      for (uint8_t s = 0; s < out_strips; s++) {
+        chaser(led_tmplt, strip_len, gIndex + out_offset * s);
+        if (s % 2 == 0) {
+          out_leds(strip_len * (s + 1) - 1, s * strip_len) = led_tmplt;
+        }
+        else {
+          out_leds(s * strip_len, strip_len * (s + 1) - 1) = led_tmplt;
+        }
+      }
+
+      // Now we copy-pasta and dump the info
+      copy_pasta_dump();
+      break;
   }
 
-  // Now try filling the inner and outer shells two different colors
-  if (gAnimCounter == 1) {
-    fill_solid(in_leds, in_LED_tot, CRGB::Red);
-    fill_solid(out_leds, out_LED_tot, CRGB::Blue);
+  // We'll have to use a separate counter for these than the global index
+//      static uint8_t in_pos = 0;
+//      static uint8_t out_pos = 0;
+//
+//      // Update the positions of both the strips and reset the array
+//      EVERY_N_MILLISECONDS(200) {
+//        in_pos = (in_pos + 1) % in_strips;
+//        out_pos = (out_pos + 1) % out_strips;
+//        clear_all();
+//      }
+//
+//      // Filling the inner perimeter and outer. We offset the outer to deal
+//      // with them being in the same array
+//      fill_solid(leds(strip_len * in_pos, strip_len * (in_pos + 1) - 1), strip_len, CHSV(10, 255, maxBrightness));
+//      fill_solid(leds(in_LED_tot + strip_len * out_pos, in_LED_tot + strip_len * (out_pos + 1) - 1), strip_len, CHSV(50, 255, maxBrightness));
+//      LEDS.show();
+//      break;
 
-    copy_pasta_dump();
-  }
-
-  // Now lets see if we can send chasers all in one direction along each shell
-  if (gAnimCounter == 2) {
-    // Fill the template array first
-    chaser(led_tmplt, strip_len, gIndex);
-
-    // Now fill both the inner template
-    for (uint8_t s = 0; s < in_strips; s++) {
-      if (s % 2 == 0) {
-        in_leds(strip_len * s, strip_len * (s + 1) - 1) = led_tmplt;
-      }
-      else {
-        in_leds(strip_len * (s + 1) - 1, strip_len * s) = led_tmplt;
-      }
-    }
-
-    // Fill outer template
-    for (uint8_t s = 0; s < out_strips; s++) {
-      if (s % 2 == 0) {
-        out_leds(strip_len * (s + 1) - 1, strip_len * s) = led_tmplt;
-      }
-      else {
-        out_leds(strip_len * s, strip_len * (s + 1) - 1) = led_tmplt;
-      }
-    }
-
-    // Now send to the LEDs
-    copy_pasta_dump();
-
-  }
 
   // Try spiraling the chasers in opposite direction on the inner and the outer
   // cylinders. Do this by offsetting the chasers by some amount to be tested later.
-  if ( gAnimCounter == 3) {
-    static uint8_t chaser_offset = 4;
-    // First do the inner cylider since it's smaller
-    for (uint8_t s = 0; s < in_strips; s++) {
-      if (s % 2 == 0) {
-        chaser(in_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + chaser_offset * s);
-      }
-      else {
-        chaser(in_leds(strip_len * (s + 1) - 1, s * strip_len), strip_len, gIndex + chaser_offset * s);
-      }
-    }
-
-    // Next do the outer cylinder
-    for (uint8_t s = 0; s < out_strips; s++) {
-      if (s % 2 == 0) {
-        chaser(out_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + chaser_offset * s);
-      }
-      else {
-        chaser(out_leds(strip_len * (s + 1) - 1, s * strip_len), strip_len, gIndex + chaser_offset * s);
-      }
-    }
-
-    // Now we copy-pasta and dump the info
-    copy_pasta_dump();
-  }
-
-  // Now lets try filling the inner and outer cylinders in opposite direction.
-  // Lets also offset them for shits and giggles AKA because it'll look badass
-  if (gAnimCounter == 4) {
-    static uint8_t chase_offset = 4;
-    // First do the inner cylinder
-    for (uint8_t s = 0; s < in_strips; s++) {
-      if (s % 2 == 0) {
-        fill_to_empty(in_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + s * chase_offset);
-      }
-      else {
-        fill_to_empty(in_leds(strip_len * (s + 1) - 1, strip_len * s), strip_len, gIndex + s * chase_offset);
-      }
-    }
-
-    // Now do the outer cylinder
-    for (uint8_t s = 0; s < out_strips; s++) {
-      if (s % 2 == 0) {
-        fill_to_empty(out_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + s * chase_offset);
-      }
-      else {
-        fill_to_empty(out_leds(strip_len * (s + 1) - 1, strip_len * s), strip_len, gIndex + s * chase_offset);
-      }
-    }
-
-    // Now copy-pasta to the main array and dump to the lights
-    copy_pasta_dump();
-  }
-
-  // Illuminate one strip at a time and send it around the perimeter. Do the same for both
-  // inner and outer, but obviously in different directions
-  if (gAnimCounter == 5) {
-    // We'll have to use a separate counter for these than the global index
-    static uint8_t in_pos = 0;
-    static uint8_t out_pos = 0;
-
-    // Update the positions of both the strips and reset the array
-    EVERY_N_MILLISECONDS(200) {
-      in_pos = (in_pos + 1) % in_strips;
-      out_pos = (out_pos + 1) % out_strips;
-      clear_all();
-    }
-
-    // Filling the inner perimeter and outer. We offset the outer to deal
-    // with them being in the same array
-    fill_solid(leds(strip_len * in_pos, strip_len * (in_pos + 1) - 1), strip_len, CRGB::Red);
-    fill_solid(leds(in_LED_tot + strip_len * out_pos, in_LED_tot + strip_len * (out_pos + 1) - 1), strip_len, CRGB::Red);
-    LEDS.show();
-
-  }
+  //  if ( gAnimCounter == 3) {
+  //    static uint8_t chaser_offset = 4;
+  //    // First do the inner cylider since it's smaller
+  //    for (uint8_t s = 0; s < in_strips; s++) {
+  //      if (s % 2 == 0) {
+  //        chaser(in_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + chaser_offset * s);
+  //      }
+  //      else {
+  //        chaser(in_leds(strip_len * (s + 1) - 1, s * strip_len), strip_len, gIndex + chaser_offset * s);
+  //      }
+  //    }
+  //
+  //    // Next do the outer cylinder
+  //    for (uint8_t s = 0; s < out_strips; s++) {
+  //      if (s % 2 == 0) {
+  //        chaser(out_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + chaser_offset * s);
+  //      }
+  //      else {
+  //        chaser(out_leds(strip_len * (s + 1) - 1, s * strip_len), strip_len, gIndex + chaser_offset * s);
+  //      }
+  //    }
+  //
+  //    // Now we copy-pasta and dump the info
+  //    copy_pasta_dump();
+  //  }
+  //
+  //  // Now lets try filling the inner and outer cylinders in opposite direction.
+  //  // Lets also offset them for shits and giggles AKA because it'll look badass
+  //  if (gAnimCounter == 4) {
+  //    static uint8_t chase_offset = 4;
+  //    // First do the inner cylinder
+  //    for (uint8_t s = 0; s < in_strips; s++) {
+  //      if (s % 2 == 0) {
+  //        fill_to_empty(in_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + s * chase_offset);
+  //      }
+  //      else {
+  //        fill_to_empty(in_leds(strip_len * (s + 1) - 1, strip_len * s), strip_len, gIndex + s * chase_offset);
+  //      }
+  //    }
+  //
+  //    // Now do the outer cylinder
+  //    for (uint8_t s = 0; s < out_strips; s++) {
+  //      if (s % 2 == 0) {
+  //        fill_to_empty(out_leds(s * strip_len, strip_len * (s + 1) - 1), strip_len, gIndex + s * chase_offset);
+  //      }
+  //      else {
+  //        fill_to_empty(out_leds(strip_len * (s + 1) - 1, strip_len * s), strip_len, gIndex + s * chase_offset);
+  //      }
+  //    }
+  //
+  //    // Now copy-pasta to the main array and dump to the lights
+  //    copy_pasta_dump();
+  //  }
+  //
+  //  // Illuminate one strip at a time and send it around the perimeter. Do the same for both
+  //  // inner and outer, but obviously in different directions
+  //  if (gAnimCounter == 5) {
+  //    // We'll have to use a separate counter for these than the global index
+  //    static uint8_t in_pos = 0;
+  //    static uint8_t out_pos = 0;
+  //
+  //    // Update the positions of both the strips and reset the array
+  //    EVERY_N_MILLISECONDS(200) {
+  //      in_pos = (in_pos + 1) % in_strips;
+  //      out_pos = (out_pos + 1) % out_strips;
+  //      clear_all();
+  //    }
+  //
+  //    // Filling the inner perimeter and outer. We offset the outer to deal
+  //    // with them being in the same array
+  //    fill_solid(leds(strip_len * in_pos, strip_len * (in_pos + 1) - 1), strip_len, CRGB::Red);
+  //    fill_solid(leds(in_LED_tot + strip_len * out_pos, in_LED_tot + strip_len * (out_pos + 1) - 1), strip_len, CRGB::Red);
+  //    LEDS.show();
+  //
+  //  }
 
   // Draw a circle on the inner and outer perimeters and move it up and down in...
   // you guessed it, opposite directions!
