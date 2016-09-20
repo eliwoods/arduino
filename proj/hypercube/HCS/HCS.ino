21b67882eaae93f8917c7b0391f584676c9ef258
Eli-Woods <eawoods3@gmail.com>
2016-09-20 00:05:39 -0700

    new blah 2

--- proj/hypercube/HCS/HCS.ino
+++ proj/hypercube/HCS/HCS.ino
@@ -106,7 +106,7 @@ void setup() {
   gBrightness = maxBrightness;
   gBlending = LINEARBLEND;
   iPaletteCounter = 0;
-  oPaletteCounter = 5;
+  oPaletteCounter = 3;
   gPaletteCounter = 0;
 
   iAnimCounter = 0;
